e967ef8a9d9163678457f10804e6d35c34f6ad53
Yusif Israfilov <113928967+JosephIsrafilov@users.noreply.github.com>
2026-02-20 17:09:28 +0400

    removed dist folder
    unnecessary folder for git

--- backend/dist/auth/current-user.decorator.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const CurrentUserData: (...dataOrPipes: unknown[]) => ParameterDecorator;
--- backend/dist/users/users.dto.d.ts
+++
@@ -1,7 +0,0 @@
-import type { UserRole } from '../common/constants';
-export declare class CreateUserDto {
-    name: string;
-    email: string;
-    password: string;
-    role: UserRole;
-}
